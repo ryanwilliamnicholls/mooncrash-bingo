7b2419e4deb39de46780744311dd1e4ae7142a20
Ryan <Ryan@192.168.20.9>
2019-01-21 21:16:43 +1000

    adding more bingo cards and moving some cards to different difficulties

--- script/prey-mooncrash-bingo.js
+++ script/prey-mooncrash-bingo.js
@@ -4,7 +4,11 @@ bingoList[1] = [
   { name: "Unlock Riley Yu", types: ["Unlock", "Find", "Riley"] },
   { name: "Find Angela Wagner", types: ["Find"] },
   { name: "Unlock Joan Winslow", types: ["Unlock", "Joan"] },
-  { name: "Escape Pod Escape", types: ["Escape"] }
+  { name: "Escape Pod Escape", types: ["Escape"] },
+  { name: "Kill a normal Mimic", types: ["Mimic"] },
+  { name: "Kill a corrupted Operator", types: ["Operator"] },
+  { name: "Die to a corrupted Operator", types: ["Operator"] },
+  { name: "Discover all Four Pytheas Moonbase Areas", types: [] }
 ];
 bingoList[2] = [
   { name: "Find Oliver Mendy", types: ["Find"] },
@@ -13,7 +17,14 @@ bingoList[2] = [
   { name: "Find Ursula Mackey", types: ["Find"] },
   { name: "Find Joe Blank", types: ["Find"] },
   { name: "Find Lauren Ladd", types: ["Find"] },
-  { name: "Find Ramon Delgado", types: ["Find"] }
+  { name: "Find Ramon Delgado", types: ["Find"] },
+  { name: "Kill a normal Phantom", types: ["Phantom"] },
+  { name: "Die to a normal Mimic", types: ["Mimic"] },
+  { name: "Die to a Voltaic Mimic", types: ["Mimic"] },
+  { name: "Die to a normal Phantom", types: ["Phantom"] },
+  { name: "Get to Corruption 1 or higher", types: ["Corruption"] },
+  { name: "Scan a normal Mimic", types: ["Mimic"] },
+  { name: "Discover all Five Escape Routes", types: [] }
 ];
 bingoList[3] = [
   { name: "Find Luis Carlos Safra", types: ["Find"] },
@@ -22,7 +33,11 @@ bingoList[3] = [
   { name: "Find Kim Jung-Jin", types: ["Find"] },
   { name: "Find Kristina Strandhall", types: ["Find"] },
   { name: "Find Sam Lopez", types: ["Find"] },
-  { name: "Find Carrol Parker-Lewis", types: ["Find"] }
+  { name: "Find Carrol Parker-Lewis", types: ["Find"] },
+  { name: "Kill a Voltaic Mimic", types: ["Mimic"] },
+  { name: "Die to a Thermal Mimic", types: ["Mimic"] },
+  { name: "Die to a Voltaic Phantom", types: ["Phantom"] },
+  { name: "Scan a normal Phantom", types: ["Phantom"] }
 ];
 bingoList[4] = [
   { name: "Find Allie McDonald", types: ["Find"] },
@@ -31,7 +46,12 @@ bingoList[4] = [
   { name: "Find Caroline Russel", types: ["Find"] },
   { name: "Find Craig Killam", types: ["Find"] },
   { name: "Find Dale Powell", types: ["Find"] },
-  { name: "Find Donny Sandoval", types: ["Find"] }
+  { name: "Find Donny Sandoval", types: ["Find"] },
+  { name: "Kill a Thermal Mimic", types: ["Mimic"] },
+  { name: "Kill a Voltaic Phantom", types: ["Phantom"] },
+  { name: "Die to an Etheric Mimic", types: ["Mimic"] },
+  { name: "Die to a Thermal Phantom", types: ["Phantom"] },
+  { name: "Scan a Voltaic Mimic", types: ["Mimic"] }
 ];
 bingoList[5] = [
   { name: "Find Patrice Young", types: ["Find"] },
@@ -41,7 +61,13 @@ bingoList[5] = [
   { name: "Find Steve R. Gain", types: ["Find"] },
   { name: "Find Albert Leeds", types: ["Find"] },
   { name: "Find Gail Stewart", types: ["Find"] },
-  { name: "Find Haley Sanders", types: ["Find"] }
+  { name: "Find Haley Sanders", types: ["Find"] },
+  { name: "Kill an Etheric Mimic", types: ["Mimic"] },
+  { name: "Kill a Thermal Phantom", types: ["Phantom"] },
+  { name: "Die to an Etheric Phantom", types: ["Phantom"] },
+  { name: "Scan a Thermal Mimic", types: ["Mimic"] },
+  { name: "Scan a Voltaic Phantom", types: ["Phantom"] },
+  { name: "Complete 5 KASMA Orders", types: ["Orders"] }
 ];
 bingoList[6] = [
   { name: "Find Andrew T. Bowman", types: ["Find"] },
@@ -50,7 +76,11 @@ bingoList[6] = [
   { name: "Find Jens Brehmer", types: ["Find"] },
   { name: "Find Johannes Kruse", types: ["Find"] },
   { name: "Find Hugo Bettens", types: ["Find"] },
-  { name: "Find Joey Bell", types: ["Find"] }
+  { name: "Find Joey Bell", types: ["Find"] },
+  { name: "Kill an Etheric Phantom", types: ["Phantom"] },
+  { name: "Die to a Moonshark", types: ["Moonshark"] },
+  { name: "Scan an Etheric Mimic", types: ["Mimic"] },
+  { name: "Scan a Thermal Phantom", types: ["Phantom"] }
 ];
 bingoList[7] = [
   { name: "Find Demarcus Cloud", types: ["Find"] },
@@ -59,7 +89,10 @@ bingoList[7] = [
   { name: "Find Grey Blessing", types: ["Find"] },
   { name: "Find Israel Kouyate", types: ["Find"] },
   { name: "Find Kathy Cheema", types: ["Find"] },
-  { name: "Find Ken Mizuki", types: ["Find"] }
+  { name: "Find Ken Mizuki", types: ["Find"] },
+  { name: "Die to a Nightmare", types: ["Nightmare"] },
+  { name: "Die to a normal Harvester", types: ["Harvester"] },
+  { name: "Scan an Etheric Phantom", types: ["Phantom"] }
 ];
 bingoList[8] = [
   { name: "Find Noel Benson", types: ["Find"] },
@@ -68,7 +101,10 @@ bingoList[8] = [
   { name: "Find Taka Sanada", types: ["Find"] },
   { name: "Find Terri Brown", types: ["Find"] },
   { name: "Find Kenneth McAlister", types: ["Find"] },
-  { name: "Find Kristen Schwanholz", types: ["Find"] }
+  { name: "Find Kristen Schwanholz", types: ["Find"] },
+  { name: "Die to a Weaver", types: ["Weaver"] },
+  { name: "Scan a Nightmare", types: ["Nightmare"] },
+  { name: "Scan a normal Harvester", types: ["Harvester"] }
 ];
 bingoList[9] = [
   { name: "Find Brian Chung", types: ["Find"] },
@@ -77,7 +113,11 @@ bingoList[9] = [
   { name: "Find Kailash Rahman", types: ["Find"] },
   { name: "Find Kelly Bhoyar", types: ["Find"] },
   { name: "Find Lea Tevet", types: ["Find"] },
-  { name: "Find Liu Mok", types: ["Find"] }
+  { name: "Find Liu Mok", types: ["Find"] },
+  { name: "Kill a Weaver", types: ["Weaver"] },
+  { name: "Scan a Weaver", types: ["Weaver"] },
+  { name: "Mass Driver Escape", types: ["Escape"] },
+  { name: "Shuttle Escape", types: ["Escape"] }
 ];
 bingoList[10] = [
   { name: "Find Alejandro Marin", types: ["Find"] },
@@ -87,6 +127,8 @@ bingoList[10] = [
   { name: "Find Dianna Riendeau", types: ["Find"] },
   { name: "Find Marjorie Smythe", types: ["Find"] },
   { name: "Find Max Cotman", types: ["Find"] }
+  { name: "Die to a Telepath", types: ["Telepath"] },
+  { name: "Scan a Moonshark", types: ["Moonshark"] }
 ];
 bingoList[11] = [
   { name: "Find Li Phang", types: ["Find"] },
@@ -95,7 +137,8 @@ bingoList[11] = [
   { name: "Find Mickey Peppers", types: ["Find"] },
   { name: "Find Nathan Leong", types: ["Find"] },
   { name: "Find Meng Tsai", types: ["Find"] },
-  { name: "Find Laurel Savage", types: ["Find"] }
+  { name: "Find Laurel Savage", types: ["Find"] },
+  { name: "Scan a Telepath", types: ["Telepath"] },
 ];
 bingoList[12] = [
   { name: "Find Harry Conway", types: ["Find"] },
@@ -120,85 +163,110 @@ bingoList[14] = [
   { name: "Find Rick Dawson", types: ["Find"] },
   { name: "Find Robbie Murphy", types: ["Find"] },
   { name: "Find Theodore Rabiot", types: ["Find"] },
-  { name: "Find Vasco Marrone", types: ["Find"] }
+  { name: "Find Vasco Marrone", types: ["Find"] },
+  { name: "Get to Corruption 2 or higher", types: ["Corruption"] },
+  { name: "Complete 7 KASMA Orders", types: ["Orders"] }
 ];
 bingoList[15] = [
   { name: "Find Myesha Abram", types: ["Find"] },
   { name: "Find Ricky Lopez", types: ["Find"] },
   { name: "Find Robert \"Scooter\" Tate", types: ["Find"] },
   { name: "Find Rose Waterfield", types: ["Find"] },
-  { name: "Find Trey Larson", types: ["Find"] }
+  { name: "Find Trey Larson", types: ["Find"] },
+  { name: "Die to a corrupted Harvester", types: ["Harvester"] },
+  { name: "Die to a Poltergeist", types: ["Poltergeist"] }
 ];
 bingoList[16] = [
   { name: "Find Larry Dixon", types: ["Find"] },
   { name: "Find Louis Sullivan Jr.", types: ["Find"] },
   { name: "Find Marcus Lee Crosby", types: ["Find"] },
   { name: "Find Margret McCandles", types: ["Find"] },
-  { name: "Find Mary Quail", types: ["Find"] }
+  { name: "Find Mary Quail", types: ["Find"] },
+  { name: "Unlock all Human abilities for Andrius", types: ["Neuromod", "Andrius"] },
+  { name: "Die to a Technopath", types: ["Technopath"] }
+  { name: "Complete 8 KASMA Orders", types: ["Orders"] }
 ];
 bingoList[17] = [
   { name: "Find Alistair Tramontin", types: ["Find"] },
   { name: "Find Gary Glass", types: ["Find"] },
   { name: "Find Janelle Parsons", types: ["Find"] },
   { name: "Find Alessandra Vega", types: ["Find"] },
-  { name: "Find Oscar Pendelton", types: ["Find"] }
+  { name: "Find Oscar Pendelton", types: ["Find"] },
+  { name: "Unlock all Human abilities for Riley", types: ["Neuromod", "Riley"] }
+  { name: "Scan a Technopath", types: ["Technopath"] }
 ];
 bingoList[18] = [
   { name: "Find Florence Billings", types: ["Find"] },
   { name: "Find Casey Fletcher", types: ["Find"] },
   { name: "Find Douglas Tanner", types: ["Find"] },
   { name: "Find Keith Welles", types: ["Find"] },
-  { name: "Mass Driver Escape", types: ["Escape"] },
-  { name: "Shuttle Escape", types: ["Escape"] }
+  { name: "Unlock all Human abilities for Vijay", types: ["Neuromod", "Vijay"] },
+  { name: "Complete 9 KASMA Orders", types: ["Orders"] }
 ];
 bingoList[19] = [
   { name: "Consciousness Emulations Escape", types: ["Escape"] },
   { name: "Mimic Portal Escape", types: ["Escape"] },
-  { name: "Complete Vijayâ€™s Story mission", types: ["Story", "Vijay", "Claire"] },
+  { name: "Complete Vijay’s Story mission", types: ["Story", "Vijay", "Claire"] },
   { name: "Unlock Claire Whitten", types: ["Story", "Unlock", "Vijay", "Claire"] },
-  { name: "Scan 5 unique Typhon with the Psychoscope", types: [""] }
+  { name: "Scan 5 unique Typhon with the Psychoscope", types: [] },
+  { name: "Kill a Poltergeist", types: ["Poltergeist"] }
 ];
 bingoList[20] = [
-  { name: "Kill a Nightmare", types: ["Kill"] },
-  { name: "Kill a Moonshark", types: ["Kill"] },
+  { name: "Kill a Nightmare", types: ["Nightmare"] },
+  { name: "Kill a Moonshark", types: ["Moonshark"] },
   { name: "Unlock all Characters", types: ["Unlock"] },
-  { name: "Find 10 unique Fabrication plans", types: ["Items"] },
-  { name: "Complete Joanâ€™s Story mission", types: ["Story", "Joan"] },
-  { name: "Find 10 unique Chipsets", types: ["Items"] }
+  { name: "Find 10 unique Fabrication plans", types: [] },
+  { name: "Complete Joan’s Story mission", types: ["Story", "Joan"] },
+  { name: "Find 10 unique Chipsets", types: [] },
+  { name: "Kill a normal Harvester", types: ["Harvester"] },
+  { name: "Complete 10 KASMA Orders", types: ["Orders"] }
 ];
 bingoList[21] = [
-  { name: "Acquire a Gluu Gun", types: ["Items"] },
-  { name: "Acquire a Huntress Boltcaster", types: ["Items"] },
-  { name: "Acquire a Q-Beam", types: ["Items"] },
-  { name: "Kill a corrupted Harvester", types: ["Kill"] },
-  { name: "Kill a Telepath", types: ["Kill"] }
+  { name: "Acquire a Gluu Gun", types: [] },
+  { name: "Acquire a Huntress Boltcaster", types: [] },
+  { name: "Acquire a Q-Beam", types: [] },
+  { name: "Acquire the ARTX PROPULSION GEN 1 chipset", types: [] },
+  { name: "Kill a corrupted Harvester", types: ["Harvester"] },
+  { name: "Kill a Telepath", types: ["Telepath"] },
+  { name: "Kill a Technopath", types: ["Technopath"] }
 ];
 bingoList[22] = [
-  { name: "Complete Rileyâ€™s Story mission", types: ["Story", "Riley" ]},
-  { name: "Max out corruption", types: [""] },
-  { name: "Remove power from all trams and areas at the same time", types: [""] },
-  { name: "Read all of the second starbender series", types: [""] },
-  { name: "Complete Claireâ€™s Story Mission", types: ["Story", "Claire"] }
+  { name: "Complete Riley’s Story mission", types: ["Story", "Riley" ]},
+  { name: "Unlock all Human abilities for Claire", types: ["Neuromod", "Claire"] },
+  { name: "Remove power from all trams and areas at the same time", types: [] },
+  { name: "Read all of the second starbender series", types: [] },
+  { name: "Complete Claire’s Story Mission", types: ["Story", "Claire"] },
+  { name: "Scan a Poltergeist", types: ["Poltergeist"] },
+  { name: "Complete 12 KASMA Orders", types: ["Orders"] }
 ];
 bingoList[23] = [
-  { name: "Install 50 neuromods across all 5 crew members", types: ["Neuromod"] },
-  { name: "Unlock all abilities for Andrius", types: ["Neuromod", "Andrius"] },
-  { name: "Kill all 5 characters in a single simulation.", types: [""] },
+  { name: "Unlock all Typhon abilities for Andrius", types: ["Neuromod", "Andrius"] },
+  { name: "Unlock all Typhon abilities for Claire", types: ["Neuromod", "Claire"] },
+  { name: "Kill all 5 characters in a single simulation.", types: [] },
   { name: "Complete Andrius Story Mission", types: ["Story", "Andrius"] },
-  { name: "Find all Crater Crew members", types: ["Find"] }
+  { name: "Find all Crater Crew members", types: ["Find"] },
+  { name: "Get to Corruption 3 or higher", types: ["Corruption"] }
 ];
 bingoList[24] = [
-  { name: "Unlock all abilities for Vijay", types: ["Neuromod", "Vijay"] },
-  { name: "Unlock all abilities for Joan", types: ["Neuromod", "Joan"] },
-  { name: "Unlock all abilities for Claire", types: ["Neuromod", "Claire"] },
-  { name: "Unlock all abilities for Riley", types: ["Neuromod", "Riley" ]},
-  { name: "Escape with 5 characters in a single simulation.", types: ["Escape"] }
+  { name: "Unlock all Typhon abilities for Riley", types: ["Neuromod", "Riley"] },
+  { name: "Unlock all Human abilities for Claire", types: ["Neuromod", "Claire"] },
+  { name: "Install 50 neuromods across all 5 crew members", types: ["Neuromod"] },
+  { name: "Unlock all abilities for Andrius", types: ["Neuromod", "Andrius"] },
+  { name: "Escape with 5 characters in a single simulation.", types: ["Escape"] },
+  { name: "Get to Corruption 4 or higher", types: ["Corruption"] },
+  { name: "Get to Corruption 5", types: ["Corruption"] },
+  { name: "Complete 15 KASMA Orders", types: ["Orders"] }
 ];
 bingoList[25] = [
   { name: "Unlock all Human abilities for all characters.", types: ["Neuromod"] },
   { name: "Unlock all Typhon abilities for all characters.", types: ["Neuromod"] },
   { name: "Find all Crew annex Crew members", types: ["Find"] },
   { name: "Find all Moonworks Crew members", types: ["Find"] },
-  { name: "Find all Pytheas labs Crew members", types: ["Find"] }
+  { name: "Find all Pytheas labs Crew members", types: ["Find"] },
+  { name: "Unlock all abilities for Vijay", types: ["Neuromod", "Vijay"] },
+  { name: "Unlock all abilities for Joan", types: ["Neuromod", "Joan"] },
+  { name: "Unlock all abilities for Claire", types: ["Neuromod", "Claire"] },
+  { name: "Unlock all abilities for Riley", types: ["Neuromod", "Riley" ]},
+  { name: "Max out corruption (Corruption 6)", types: ["Corruption"] }
 ];
 $(function() { srl.bingo(bingoList, 5); });
